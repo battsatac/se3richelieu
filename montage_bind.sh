7a20e8225925c600320f20dfbd59a3e651e426c1
root <root@se3.Richelieu>
2018-09-16 11:24:31 +0200

    modif git path

--- montage_bind.sh
+++ montage_bind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mount --bind /root/batts/github/clients-linux/unefois/ /home/netlogon/clients-linux/unefois
-mount --bind /root/batts/github/clients-linux/bin/ /home/netlogon/clients-linux/bin
+mount --bind /root/batts/github/se3richelieu/clients-linux/unefois/ /home/netlogon/clients-linux/unefois
+mount --bind /root/batts/github/se3richelieu/clients-linux/bin/ /home/netlogon/clients-linux/bin
